7f3875228dcbabfa767d6d4181ef78c6f8b4483d
André A. Prado <andre.prado@egsys.com.br>
2026-03-11 12:49:38 -0300

    cleanup: Remover código-fonte do repositório público

--- __init__.py
+++
@@ -1,3 +0,0 @@
-"""egSYS Tool - Sistema de Gerenciamento Multi-Servidor."""
-__version__ = "2.0.0"
-__author__ = "André Antônio Prado da Silva"
--- core/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Módulo core com lógica de negócio."""
