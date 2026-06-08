a76232e6f700eeef3a120784b5b0760efed264c7
André Prado <andre.prado@egsys.com.br>
2026-06-08 10:16:28 -0300

    chore: Auto-update installers to v2.3.4

--- install-arch.sh
+++ install-arch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# egSYS SAPA Tool - Instalador Arch Linux v2.3.3
+# egSYS SAPA Tool - Instalador Arch Linux v2.3.4
 
 set -e
 
-EGSYS_VERSION="2.3.3"
+EGSYS_VERSION="2.3.4"
 APP_NAME="egsys"
 INSTALL_DIR="/opt/egsys-tool"
 BIN_DIR="/usr/local/bin"
