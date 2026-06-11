fbff61e5a3e397e6a5be2e9e2ddbf67f18a394f3
André Prado <andre.prado@egsys.com.br>
2026-06-11 11:10:34 -0300

    chore: Auto-update installers to v2.5.1

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# egSYS SAPA Tool - Instalador Ubuntu/Debian v2.5.0
+# egSYS SAPA Tool - Instalador Ubuntu/Debian v2.5.1
 
 set -e
 
-EGSYS_VERSION="2.5.0"
+EGSYS_VERSION="2.5.1"
 APP_NAME="egsys"
 INSTALL_DIR="/opt/egsys-tool"
 BIN_DIR="/usr/local/bin"
