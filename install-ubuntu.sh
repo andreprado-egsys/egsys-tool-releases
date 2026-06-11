2ce23786bf21b9a0e5f6e30c4687e3abe86ef957
André Prado <andre.prado@egsys.com.br>
2026-06-11 12:45:15 -0300

    chore: Auto-update installers to v2.5.3

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# egSYS SAPA Tool - Instalador Ubuntu/Debian v2.5.2
+# egSYS SAPA Tool - Instalador Ubuntu/Debian v2.5.3
 
 set -e
 
-EGSYS_VERSION="2.5.2"
+EGSYS_VERSION="2.5.3"
 APP_NAME="egsys"
 INSTALL_DIR="/opt/egsys-tool"
 BIN_DIR="/usr/local/bin"
